292b342e0416905a32dc322a6e72d29ff1a51036
Jonty <jontyclark2005@gmail.com>
2025-08-08 18:55:48 +1200

    LIMIT SWITCHES ALL WORK, DON'T F$*K THIS S&@T UP!

--- src/main.cpp/limit_switch.cpp
+++ src/main.cpp/limit_switch.cpp
@@ -2,14 +2,21 @@
 #include <avr/interrupt.h>
 #include "limit_switch.hpp"
 
-Limit_Switch::Limit_Switch(volatile uint8_t *ddr, volatile uint8_t *pinr, uint8_t bit){
+Limit_Switch::Limit_Switch(volatile uint8_t *ddr, volatile uint8_t *pinr, volatile uint8_t *port, uint8_t bit){
     // Set the pin as input
     this->ddr_register = ddr;
     this->pin_register = pinr;
+    this->port_register = port;
     this->bit = bit;
+    *(this->ddr_register) &= ~(1 << bit); // Clear the bit to set as input
+    *(this->port_register) |= (1 << bit); // Enable pull-up resistor
+    // Note: The port register is used to enable the pull-up resistor
+    // This assumes the pin is active low, meaning it reads low when pressed
 }
 
 bool Limit_Switch::is_pressed() {
-    return this->pin_register & (1 << bit);
+    return (*(this->pin_register) & (1 << bit)) != 0;
+    // Returns true if the pin is low (pressed)
+    // Returns false if the pin is high (not pressed)
 }
 
--- src/main.cpp/limit_switch.hpp
+++ src/main.cpp/limit_switch.hpp
@@ -7,16 +7,13 @@
 
 class Limit_Switch {
     public:
-        Limit_Switch(volatile uint8_t *ddr, volatile uint8_t *pinr, uint8_t bit){
-            this->ddr_register = ddr;
-            this->pin_register = pinr;
-            this->bit = bit;
-        }
+        Limit_Switch(volatile uint8_t *ddr, volatile uint8_t *pinr, volatile uint8_t *port, uint8_t bit);
         bool is_pressed();
 
     private:
         volatile uint8_t *pin_register;
         volatile uint8_t *ddr_register;
+        volatile uint8_t *port_register;
         uint8_t bit;
 };
 
