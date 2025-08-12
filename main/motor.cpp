16b2e156f115b30f1155e2daf49277d38d020cf9
rhyschit <rchi339@aucklanduni.ac.nz>
2025-08-12 15:24:05 +1200

    Motor and encoder combined

--- main/motor.cpp
+++ main/motor.cpp
@@ -1,12 +1,17 @@
 #include "motor.hpp"
 #include <avr/io.h>
-#include "encoder.hpp"
+
+Motor* Motor::motor1 = nullptr;
+Motor* Motor::motor2 = nullptr;
 
 Motor::Motor(int voltage, Timer timer, int pwm_pin, int enc_a_pin, int enc_b_pin)
   : voltage(voltage), timer(timer), pwm_pin(pwm_pin), enc_a_pin(enc_a_pin), enc_b_pin(enc_b_pin) {
 
+  DDRD &= ~(1 << enc_a_pin);
+  DDRD &= ~(1 << enc_b_pin);
+
   switch (timer) {
-    case Timer::TIMER3A:
+    case Timer::M1_TIMER3A:
       // PE3 → OC3A → D5
       DDRE |= (1 << PE3);  // Set PE3 as output
 
@@ -14,25 +19,66 @@ Motor::Motor(int voltage, Timer timer, int pwm_pin, int enc_a_pin, int enc_b_pin
       TCCR3A = (1 << COM3A1) | (1 << WGM30);
       TCCR3B = (1 << WGM32) | (1 << CS31); // Prescaler = 8
       OCR3A = voltage;
+
+      DDRD &= ~(1 << PD1); //Set pins 20 and 21 as inputs for encoder signals
+      DDRD &= ~(1 << PD0);
+
+      EICRA |= (1 << ISC00); //Enable INT0 on rising edge
+      EICRA &= ~(1 << ISC01);
+      EIMSK |= (1 << INT0);
+
+      pinA = enc_a_pin;
+      pinB = enc_b_pin;
+
+      motor1 = this;
       break;
 
-    case Timer::TIMER4A:
+    case Timer::M2_TIMER4A:
       // PH3 → OC4A → D6
       DDRH |= (1 << PH3);  // Set PH3 as output
 
       TCCR4A = (1 << COM4A1) | (1 << WGM40);
       TCCR4B = (1 << WGM42) | (1 << CS41); // Prescaler = 8
       OCR4A = voltage;
+
+      DDRD &= ~(1 << PD3); //Set pins 18 and 19 as inputs for encoder signals
+      DDRD &= ~(1 << PD2);
+
+      EICRA |= (1 << ISC20); //Enable INT2 on rising edge
+      EICRA &= ~(1 << ISC21);
+      EIMSK |= (1 << INT2);
+
+      pinA = enc_a_pin;
+      pinB = enc_b_pin;
+
+      motor2 = this;
       break;
   }
-
-  myEncoder = new Encoder(motorNumber);
-  
 }
 
-int Motor::get_voltage() const {
-  return voltage;
-}
+// int Motor::get_voltage() const {
+//   return voltage;
+// }
+
+
+// int read_encoder() {
+
+// }
+
+
+// void stop_motor(void) {
+//   switch (timer) {
+//     case Timer::M1_TIMER3A:
+//       OCR3A = 0;
+//       break;
+//     case Timer::M2_TIMER4A:
+//       OCR4A = 0;
+//       break;
+//   }
+// }
+
+
+
 
 void Motor::move_motor(int new_voltage) {
   if (new_voltage < 0) new_voltage = 0;
@@ -41,15 +87,49 @@ void Motor::move_motor(int new_voltage) {
   voltage = new_voltage;
 
   switch (timer) {
-    case Timer::TIMER3A:
+    case Timer::M1_TIMER3A:
       OCR3A = voltage;
       break;
-    case Timer::TIMER4A:
+    case Timer::M2_TIMER4A:
       OCR4A = voltage;
       break;
   }
 }
 
+
+// void fast_clockwise(int voltage, int ms);
+
+
+// void fast_anticlockwise(int voltage, int ms);
+
 int Motor::GetEncoderDist() {
-    return myEncoder->GetEncoderDist();
+  return int(double(encCount) * 13.5 * 3.14 / 172 / 24); // Convert counts to distance
+}
+
+void Motor::ResetEncoder() {
+  encCount = 0; // Reset the encoder count to zero
+}
+
+void Motor::incrementEncoder() {
+  uint8_t pin_state = PIND;
+  uint8_t a = (pin_state >> pinA) & 0x01;
+  uint8_t b = (pin_state >> pinB) & 0x01;
+
+  if (a == b) {
+    encCount++;
+  } else {
+    encCount--; 
+  }
+}
+
+ISR(INT0_vect) {
+  if(Motor::motor1 != nullptr) {
+    Motor::motor1->incrementEncoder();
+  }
+}
+
+ISR(INT2_vect) {
+  if(Motor::motor2 != nullptr) {
+    Motor::motor2->incrementEncoder();
+  }
 }
--- main/motor.hpp
+++ main/motor.hpp
@@ -1,12 +1,9 @@
 #ifndef MOTOR_HPP
 #define MOTOR_HPP
-#include "encoder.hpp"
-
-class Encoder;
 
 enum class Timer {
-  TIMER3A,  // PE3 → OC3A → D5
-  TIMER4A   // PH3 → OC4A → D6
+  M1_TIMER3A,  // PE3 → OC3A → D5
+  M2_TIMER4A   // PH3 → OC4A → D6
 };
 
 class Motor {
@@ -17,15 +14,29 @@ class Motor {
     int pwm_pin;
     int enc_a_pin;
     int enc_b_pin;
-    Encoder* myEncoder;
-    int motorNumber;
+
+    volatile uint16_t encCount;
 
   public:
     Motor(int voltage, Timer timer, int pwm_pin, int enc_a_pin, int enc_b_pin);
 
-    int get_voltage() const;
+    // int get_voltage() const;
+    // int read_encoder(); 
+    
+    // void stop_motor(void);
     void move_motor(int voltage);
+
+
+    // void fast_clockwise(int voltage, int ms);
+    // void fast_anticlockwise(int voltage, int ms);
+
     int GetEncoderDist();
+    void ResetEncoder();
+    void incrementEncoder();
+
+    static Motor* motor1;
+    static Motor* motor2;
+
 };
 
 #endif // MOTOR_HPP
